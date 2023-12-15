f89e6e9fd0fc0fdcc9b8be9d964780a0d68dc179
jianweiCiou <jianwei.ciou@gmail.com>
2023-12-15 17:32:57 +0800

    Create CreateSPCoinResponse.java

---
+++ connecttool/src/main/java/com/r17dame/connecttool/datamodel/CreateSPCoinResponse.java
@@ -0,0 +1,42 @@
+package com.r17dame.connecttool.datamodel;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CreateSPCoinResponse {
+    @SerializedName("data")
+    public PaymentData data;
+    @SerializedName("status")
+    public int status;
+    @SerializedName("message")
+    @Expose
+    public String message;
+    @SerializedName("detailMessage")
+    @Expose
+    public String detailMessage;
+
+    @SerializedName("requestNumber")
+    public String requestNumber;
+
+    public class PaymentData {
+        @SerializedName("transactionId")
+        public String transactionId;
+
+        @SerializedName("orderNo")
+        public String orderNo;
+        @SerializedName("spCoin")
+        public int spCoin;
+        @SerializedName("rebate")
+        public int rebate;
+        @SerializedName("orderStatus")
+        public String orderStatus;
+        @SerializedName("state")
+        public String state;
+        @SerializedName("notifyUrl")
+        @Expose
+        public String notifyUrl;
+        @SerializedName("sign")
+        @Expose
+        public String sign;
+    }
+}
