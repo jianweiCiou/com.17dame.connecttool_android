41615c800930ad3d43f510a946906cafab04f65a
jianweiCiou <jianwei.ciou@gmail.com>
2023-12-15 17:29:30 +0800

    PaymentResponse change to CreateSPCoinResponse

--- connecttool/src/main/java/com/r17dame/connecttool/callback/CreatePaymentCallback.java
+++ connecttool/src/main/java/com/r17dame/connecttool/callback/CreatePaymentCallback.java
@@ -1,9 +1,9 @@
 package com.r17dame.connecttool.callback;
 
-import com.r17dame.connecttool.datamodel.PaymentResponse;
+import com.r17dame.connecttool.datamodel.CreateSPCoinResponse;
 
 public interface CreatePaymentCallback {
 
-    void callback(PaymentResponse value);
+    void callback(CreateSPCoinResponse value);
 
 }
